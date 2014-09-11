2cf6ee6590227b140ea5531d720bd9922f66d8b7
Ubuntu <ubuntu@ip-10-181-236-150.ec2.internal>
2014-09-11 15:00:09 +0000

    fixed up clear-pick script

--- scripts/clear-pick.js
+++ scripts/clear-pick.js
@@ -1,15 +1,26 @@
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
     
-var week = process.argv[2];
+var week = +process.argv[2];
 var user = process.argv[3];
 
+console.log("week: " + week);
+console.log("user: " + user);
   MongoClient.connect('mongodb://127.0.0.1:27017/picks', function(err, db) {
     if(err) throw err;
     
-    var collection = db.collection('games');
-    collection.find({week: week}).forEach(function(doc){
-      delete doc.picks[user];
-      collection.update({_id: doc._id},{$set:{picks:doc.picks}});
-    )};
+    db.collection('games', function(err, collection){
+    collection.find({week: week}, function(err, cursor){
+	cursor.toArray(function(err, docs){
+		docs.map(function(doc){
+        		console.log(doc);
+			if(doc.picks[user]){
+				delete doc.picks[user];
+			}
+      			collection.update({_id: doc._id},{$set:{picks:doc.picks}}, function(){console.log("Updated");});
+    		});
+	});
+    });
+    });   
+    console.log("Update successfull");
 });
